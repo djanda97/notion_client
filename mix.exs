da41e1cef1c7b19f91f94dc3d838d2ead7391e6e
David Janda <djanda97@gmail.com>
2023-04-19 17:33:03 -0700

    Add info to mix.exs in order to publish package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule NotionClient.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/djanda97/notion_client"
     ]
   end
 
@@ -21,8 +24,20 @@ defmodule NotionClient.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "An API client for Notion written in Elixir"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/djanda97/notion_client"}
     ]
   end
 end
